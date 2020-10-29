6a16c41056d2704c54751e7936a091882d8b4bd3
baharriyan <indonesiariyan@gmail.com>
2020-10-30 06:34:51 +0700

    first commit

---
+++ PBOIF2-10116416-LATIHAN25-EjaanNama/src/pboif2/pkg10116416/latihan25/ejaannama/PBOIF210116416LATIHAN25EjaanNama.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10116416.latihan25.ejaannama;
+
+/**
+ *
+ * @author Riyan bahar
+ * nim: 10116416
+ * PBOIF2
+ */
+import java.util.Scanner;
+public class PBOIF210116416LATIHAN25EjaanNama {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        
+        
+        System.out.print("Masukkan nama depan anda untuk di eja : ");
+        String namaDepan = input.next();
+        System.out.println("");
+        System.out.println("Ejaan untuk \"" + namaDepan + "\" adalah : ");
+        for(int i = 1; i <= namaDepan.length(); i++){
+            System.out.println("Huruf Ke-" + i + " : " + namaDepan.substring(i-1,i));
+        }
+        System.out.println("");
+    }
+    
+}
